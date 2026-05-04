bbb576b84760b2ee4bbca56a64f87fbdea1d1f8c
izabellyomenezes <izabelly.omenezes@gmail.com>
2026-05-04 20:37:18 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+char nome[50];
+char placa[7];
+int tipo_usuario;
+
+int hr_inicio;
+int duracao_min;
+float potencia_kw;
+float energia_kwh;
+float tarifa;
+float custo_total;
+int sessao_iniciada = 0;
+int sessao_calculada = 0;
+
+int main() {
+    
+}
