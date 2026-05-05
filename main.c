8b188c16581c2ab5ae4fa5b3d59e9cad2bab19d5
izabellyomenezes <izabelly.omenezes@gmail.com>
2026-05-04 21:48:56 -0300

    add swtich and while

--- main.c
+++ main.c
@@ -14,5 +14,37 @@ int sessao_iniciada = 0;
 int sessao_calculada = 0;
 
 int main() {
-    
+    int opcao;
+
+    printf("\n========================= \n");
+    printf("= SISTEMA SPRINT 1 =\n");
+    printf("=========================\n");
+
+    while(opcao != 5){
+        printf("1 - INSERIR INFORMACOES DO USUARIO\n2 - INSERIR INFORMACOES DO CARRO\n3 - CALCULA SESSAO\n4 - EXIBE RESULTADO\n5 - SAIR\nDigite a opcao desejada: ");
+
+        if (scanf("%d", &opcao) != 1){
+            printf("OPCAO INVALIDA\n");
+            while(getchar() != '\n')
+                continue;
+            continue;
+        }
+
+        switch(opcao){
+            case 1: //Adiciona informacoes sobre usuario
+                break;
+            case 2: //Adiciona informacoes sobre o carro
+                break;
+            case 3: //Calcula sessao
+                break;
+            case 4: //Exibe resultado
+                break;
+            case 5:
+                printf("FINALIZANDO...\n");
+                break;
+            default:
+                printf("OPCAO INVALIDA\n");
+        }
+    }
+    return 0;
 }
